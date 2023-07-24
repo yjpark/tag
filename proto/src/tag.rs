6c0369d6d9815ee26cf4578e214cd424a0c04222
YJ Park <yjpark@gmail.com>
2023-07-25 00:43:50 +0800

    🚧 [WIP] - 2023-07-25 00:43:50 -

--- proto/src/tag.rs
+++ proto/src/tag.rs
@@ -1,8 +1,21 @@
-use downcast_rs::{DowncastSync, impl_downcast};
+use downcast_rs::impl_downcast;
+#[cfg(feature = "arc")]
+use downcast_rs::DowncastSync;
+#[cfg(not(feature = "arc"))]
+use downcast_rs::Downcast;
 
 use crate::prelude::{Uuid, CoreTag};
 
+#[cfg(feature = "arc")]
 pub trait Tag : CoreTag + DowncastSync {
     fn parent(&self) -> Option<Uuid>;
 }
+#[cfg(feature = "arc")]
 impl_downcast!(sync Tag);
+
+#[cfg(not(feature = "arc"))]
+pub trait Tag : CoreTag + Downcast {
+    fn parent(&self) -> Option<Uuid>;
+}
+#[cfg(not(feature = "arc"))]
+impl_downcast!(Tag);
