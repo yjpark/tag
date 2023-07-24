503376024ecbb0058c7e9a94eb75c82890e5b1b8
YJ Park <yjpark@gmail.com>
2023-07-25 00:27:43 +0800

    🚧 [WIP] - 2023-07-25 00:27:43 -

--- proto/src/tag.rs
+++ proto/src/tag.rs
@@ -1,5 +1,8 @@
+use downcast_rs::{DowncastSync, impl_downcast};
+
 use crate::prelude::{Uuid, CoreTag};
 
-pub trait Tag : CoreTag {
+pub trait Tag : CoreTag + DowncastSync {
     fn parent(&self) -> Option<Uuid>;
 }
+impl_downcast!(sync Tag);
